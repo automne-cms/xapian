4c8f09507ee30ebaa85c459f2d657503c986f589
sebastien <sebastien>
2007-09-20 09:30:12 +0000

    0.2 [REFACTOR] : Change license text

--- automne/classes/modules/ase/filters/common.php
+++ automne/classes/modules/ase/filters/common.php
@@ -5,19 +5,15 @@
 // +----------------------------------------------------------------------+
 // | Copyright (c) 2000-2007 WS Interactive                               |
 // +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the GPL license,       |
-// | or (at your discretion) to version 3.0 of the PHP license.           |
-// | The first is bundled with this package in the file LICENSE-GPL, and  |
-// | is available at through the world-wide-web at                        |
+// | This source file is subject to version 2.0 of the GPL license.       |
+// | The license text is bundled with this package in the file            |
+// | LICENSE-GPL, and is available at through the world-wide-web at       |
 // | http://www.gnu.org/copyleft/gpl.html.                                |
-// | The later is bundled with this package in the file LICENSE-PHP, and  |
-// | is available at through the world-wide-web at                        |
-// | http://www.php.net/license/3_0.txt.                                  |
 // +----------------------------------------------------------------------+
 // | Author: Sébastien Pauchet <sebastien.pauchet@ws-interactive.fr>      |
 // +----------------------------------------------------------------------+
 //
-// $Id: common.php,v 1.1.1.1 2007/09/04 15:01:29 sebastien Exp $
+// $Id: common.php,v 1.2 2007/09/20 09:30:12 sebastien Exp $
 
 /**
   * Class CMS_filter_common
--- automne/classes/modules/ase/xapianDB.php
+++ automne/classes/modules/ase/xapianDB.php
@@ -5,19 +5,15 @@
 // +----------------------------------------------------------------------+
 // | Copyright (c) 2000-2007 WS Interactive                               |
 // +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the GPL license,       |
-// | or (at your discretion) to version 3.0 of the PHP license.           |
-// | The first is bundled with this package in the file LICENSE-GPL, and  |
-// | is available at through the world-wide-web at                        |
+// | This source file is subject to version 2.0 of the GPL license.       |
+// | The license text is bundled with this package in the file            |
+// | LICENSE-GPL, and is available at through the world-wide-web at       |
 // | http://www.gnu.org/copyleft/gpl.html.                                |
-// | The later is bundled with this package in the file LICENSE-PHP, and  |
-// | is available at through the world-wide-web at                        |
-// | http://www.php.net/license/3_0.txt.                                  |
 // +----------------------------------------------------------------------+
 // | Author: Sébastien Pauchet <sebastien.pauchet@ws-interactive.fr>      |
 // +----------------------------------------------------------------------+
 //
-// $Id: xapianDB.php,v 1.5 2007/09/11 08:03:29 sebastien Exp $
+// $Id: xapianDB.php,v 1.6 2007/09/20 09:30:11 sebastien Exp $
 
 /**
   * Class CMS_XapianDB
