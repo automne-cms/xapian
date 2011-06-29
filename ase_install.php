2fa5a88c74f296879e800c984ac46c9bfe6d2e16
sebastien-pauchet <seb@automne-cms.org>
2011-06-29 16:35:30 +0200

    [BUG] : Correct a bug in installation process if Automne is installed in a subfolder

--- ase_install.php
+++ ase_install.php
@@ -55,70 +55,76 @@ if (!$installed) {
 		$q = new CMS_query('ALTER TABLE  mod_ase_document CHANGE  language_mased  language_mased CHAR( 5 ) NOT NULL');
 		echo 'Database successfuly updated (handle language codes of 5 characters)<br/>';
 	}
-	
-	//load destination module parameters
-	$module = CMS_modulesCatalog::getByCodename('ase');
-	$moduleParameters = $module->getParameters(false,true);
-	if (!is_array($moduleParameters)) {
-		$moduleParameters = array();
-	}
-	//load the XML data of the source the files
-	$sourceXML = new CMS_file(PATH_TMP_FS.PATH_PACKAGES_WR.'/modules/ase_rc.xml');
-	$domdocument = new CMS_DOMDocument();
-	try {
-		$domdocument->loadXML($sourceXML->readContent("string"));
-	} catch (DOMException $e) {}
-	$paramsTags = $domdocument->getElementsByTagName('param');
-	$sourceParameters = array();
-	foreach ($paramsTags as $aTag) {
-		$name = ($aTag->hasAttribute('name')) ? $aTag->getAttribute('name') : '';
-		$type = ($aTag->hasAttribute('type')) ? $aTag->getAttribute('type') : '';
-		$sourceParameters[$name] = array(CMS_DOMDocument::DOMElementToString($aTag, true),$type);
-	}
-	//merge the two tables of parameters
-	$resultParameters = array_merge($sourceParameters,$moduleParameters);
-	//set new parameters to the module
-	if ($module->setAndWriteParameters($resultParameters)) {
-		echo 'Modules parameters successfully merged<br />';
-		//move databases if needed
-		if (is_dir(PATH_MODULES_FILES_FS.'/'.MOD_ASE_CODENAME.'/databases/')) {
-			//create new directory
-			$errorUpdate = false;
-			if (!is_dir(PATH_MAIN_FS."/".MOD_ASE_CODENAME.'/databases/')) {
-				if (!CMS_file::makeDir(PATH_MAIN_FS."/".MOD_ASE_CODENAME.'/databases/')) {
-					$errorUpdate = true;
-					echo 'Error : Cannot create directory '.PATH_MAIN_WR.'/'.MOD_ASE_CODENAME.'/databases/<br />';
+	//check modules parameters
+	if (!file_exists(PATH_PACKAGES_FS.'/modules/ase_rc.xml')) {
+		CMS_file::copyTo(PATH_TMP_FS.'/automne/tmp/modules/ase_rc.xml',PATH_PACKAGES_FS.'/modules/ase_rc.xml');
+		CMS_file::chmodFile(FILES_CHMOD, PATH_PACKAGES_FS.'/modules/ase_rc.xml');
+		echo "ASE installation : Update done.<br /><br />";
+	} else {
+		//load destination module parameters
+		$module = CMS_modulesCatalog::getByCodename('ase');
+		$moduleParameters = $module->getParameters(false,true);
+		if (!is_array($moduleParameters)) {
+			$moduleParameters = array();
+		}
+		//load the XML data of the source the files
+		$sourceXML = new CMS_file(PATH_TMP_FS.PATH_PACKAGES_WR.'/modules/ase_rc.xml');
+		$domdocument = new CMS_DOMDocument();
+		try {
+			$domdocument->loadXML($sourceXML->readContent("string"));
+		} catch (DOMException $e) {}
+		$paramsTags = $domdocument->getElementsByTagName('param');
+		$sourceParameters = array();
+		foreach ($paramsTags as $aTag) {
+			$name = ($aTag->hasAttribute('name')) ? $aTag->getAttribute('name') : '';
+			$type = ($aTag->hasAttribute('type')) ? $aTag->getAttribute('type') : '';
+			$sourceParameters[$name] = array(CMS_DOMDocument::DOMElementToString($aTag, true),$type);
+		}
+		//merge the two tables of parameters
+		$resultParameters = array_merge($sourceParameters,$moduleParameters);
+		//set new parameters to the module
+		if ($module->setAndWriteParameters($resultParameters)) {
+			echo 'Modules parameters successfully merged<br />';
+			//move databases if needed
+			if (is_dir(PATH_MODULES_FILES_FS.'/'.MOD_ASE_CODENAME.'/databases/')) {
+				//create new directory
+				$errorUpdate = false;
+				if (!is_dir(PATH_MAIN_FS."/".MOD_ASE_CODENAME.'/databases/')) {
+					if (!CMS_file::makeDir(PATH_MAIN_FS."/".MOD_ASE_CODENAME.'/databases/')) {
+						$errorUpdate = true;
+						echo 'Error : Cannot create directory '.PATH_MAIN_WR.'/'.MOD_ASE_CODENAME.'/databases/<br />';
+					}
 				}
-			}
-			//copy all files from old directory to new one if they do not already exists
-			if (!$errorUpdate) {
-				try{
-					foreach ( new RecursiveIteratorIterator(new RecursiveDirectoryIterator(PATH_MODULES_FILES_FS."/".MOD_ASE_CODENAME.'/databases'), RecursiveIteratorIterator::SELF_FIRST) as $file) {
-						if ($file->isFile() && $file->getFilename() != '.htaccess') {
-							$to = str_replace(PATH_MODULES_FILES_FS."/".MOD_ASE_CODENAME, PATH_MAIN_FS."/".MOD_ASE_CODENAME, $file->getPathname());
-							if (!file_exists($to) && !CMS_file::copyTo($file->getPathname(), $to)) {
-								//echo "Error copy on ".$file->getPathname().' -> '.$to.'<br />';
-								$errorUpdate = true;
+				//copy all files from old directory to new one if they do not already exists
+				if (!$errorUpdate) {
+					try{
+						foreach ( new RecursiveIteratorIterator(new RecursiveDirectoryIterator(PATH_MODULES_FILES_FS."/".MOD_ASE_CODENAME.'/databases'), RecursiveIteratorIterator::SELF_FIRST) as $file) {
+							if ($file->isFile() && $file->getFilename() != '.htaccess') {
+								$to = str_replace(PATH_MODULES_FILES_FS."/".MOD_ASE_CODENAME, PATH_MAIN_FS."/".MOD_ASE_CODENAME, $file->getPathname());
+								if (!file_exists($to) && !CMS_file::copyTo($file->getPathname(), $to)) {
+									//echo "Error copy on ".$file->getPathname().' -> '.$to.'<br />';
+									$errorUpdate = true;
+								}
 							}
 						}
+					} catch(Exception $e) {}
+				}
+				//remove old dir
+				if (!$errorUpdate) {
+					if (!CMS_file::deltree(PATH_MODULES_FILES_FS.'/'.MOD_ASE_CODENAME, true)) {
+						echo '/!\ To complete module update, delete directory '.PATH_MODULES_FILES_WR.'/'.MOD_ASE_CODENAME.' <br/> <br/>';
+					} else {
+						echo "ASE installation : Update done.<br /><br />";
 					}
-				} catch(Exception $e) {}
-			}
-			//remove old dir
-			if (!$errorUpdate) {
-				if (!CMS_file::deltree(PATH_MODULES_FILES_FS.'/'.MOD_ASE_CODENAME, true)) {
-					echo '/!\ To complete module update, delete directory '.PATH_MODULES_FILES_WR.'/'.MOD_ASE_CODENAME.' <br/> <br/>';
 				} else {
-					echo "ASE installation : Update done.<br /><br />";
+					echo '/!\ To complete module update, copy all files from '.PATH_MODULES_FILES_WR.'/'.MOD_ASE_CODENAME.'/databases to '.PATH_MAIN_WR."/".MOD_ASE_CODENAME.'/databases then delete directory '.PATH_MODULES_FILES_WR.'/'.MOD_ASE_CODENAME.' <br/><br/>';
 				}
 			} else {
-				echo '/!\ To complete module update, copy all files from '.PATH_MODULES_FILES_WR.'/'.MOD_ASE_CODENAME.'/databases to '.PATH_MAIN_WR."/".MOD_ASE_CODENAME.'/databases then delete directory '.PATH_MODULES_FILES_WR.'/'.MOD_ASE_CODENAME.' <br/><br/>';
+				echo "ASE installation : Update done.<br /><br />";
 			}
 		} else {
-			echo "ASE installation : Update done.<br /><br />";
+			echo "ASE installation : UPDATE ERROR ! Problem for merging modules parameters ...<br /><br />";
 		}
-	} else {
-		echo "ASE installation : UPDATE ERROR ! Problem for merging modules parameters ...<br /><br />";
 	}
 }
 $instruction = new CMS_file(PATH_TMP_FS.'/HOW_TO_INSTALL');
